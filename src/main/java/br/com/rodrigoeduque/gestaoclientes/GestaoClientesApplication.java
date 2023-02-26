0446d1b7fe9e684c4dbcf4d126eaca5a5e3dc939
Rodrigo Eustáquio Duque <rodrigoeduque@hotmail.com>
2023-02-26 11:23:39 -0300

    Foundation

---
+++ src/main/java/br/com/rodrigoeduque/gestaoclientes/GestaoClientesApplication.java
@@ -0,0 +1,13 @@
+package br.com.rodrigoeduque.gestaoclientes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestaoClientesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestaoClientesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/rodrigoeduque/gestaoclientes/GestaoClientesApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.rodrigoeduque.gestaoclientes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestaoClientesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
